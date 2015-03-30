a9a3b91393e2089e4abe5796d515add78affeddc
Josie Wright <jozwright@gmail.com>
2015-03-30 15:23:01 -0700

    Refactor

--- main_pdf.rb
+++ main_pdf.rb
@@ -9,23 +9,19 @@ class MainPdf < Prawn::Document
   def initialize
     super(page_layout: :landscape)
 
-    numbers = { 'uno' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]], 'four' => [[1, 2, 3, 4]] }, 
+    numbers = { 'uno' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]], 'four' => [[1, 2, 3, 4]], 'five' => [[1, 2, 3, 4]], 'six' => [[1, 2, 3, 4]], 'seven' => [[1, 2, 3, 4]], 'eight' => [[1, 2, 3, 4]], 'nine' => [[1, 2, 3, 4]] }, 
                 'dos' => { 'one' => [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]] }, 
                 'tres' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]] },
                 'cuatro' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]] },
                 'cinco' => { 'one' => [[1, 2, 3, 4], [1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]] },
-                'seis' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]], 'four' => [[1, 2, 3, 4]], 'five' => [[1, 2, 3, 4]], 'six' => [[1, 2, 3, 4]], 'seven' => [[1, 2, 3, 4]], 'eight' => [[1, 2, 3, 4]], 'nine' => [[1, 2, 3, 4]], 'ten' => [[1, 2, 3, 4]], 'eleven' => [[1, 2, 3, 4]], 'twelve' => [[1, 2, 3, 4]], 'thirteen' => [[1, 2, 3, 4]] }}
+                'seis' => { 'one' => [[1, 2, 3, 4]], 'two' => [[1, 2, 3, 4]], 'three' => [[1, 2, 3, 4]], 'four' => [[1, 2, 3, 4]], 'five' => [[1, 2, 3, 4]], 'six' => [[1, 2, 3, 4]], 'seven' => [[1, 2, 3, 4]], 'eight' => [[1, 2, 3, 4]], 'nine' => [[1, 2, 3, 4]], 'ten' => [[1, 2, 3, 4]], 'eleven' => [[1, 2, 3, 4]], 'twelve' => [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]], 'thirteen' => [[1, 2, 3, 4]] }}
 
     data = []
     table_data = []
-    s_needed_count = 1
 
     numbers.each do |spanish, elem|
-      s_needed_count += elem.length / 10
-      s_count = 1
       e_needed_count = 1
-      elem.each_slice(10) do |groups|
-        s_count += 1
+      elem.each_slice(5) do |groups|
         groups.each do |english, int|
           e_needed_count += int.length / 12
           e_count = 1
@@ -35,13 +31,10 @@ class MainPdf < Prawn::Document
             break if e_count > e_needed_count
             data << [{ content: spanish_table(spanish, table_data), colspan: 4 }]
             table_data.clear
-          end
+          end 
         end
         data << [{ content: spanish_table(spanish, table_data), colspan: 4 }]
         table_data.clear
-        break if s_count < s_needed_count
-        main_table(data)
-        data.clear
       end
     end
     main_table(data)
